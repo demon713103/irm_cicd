208efc651fc5562b11fe50a6a4cc08ca2adfe355
demon713103 <deep.mondal@techriskpartners.com>
2026-02-23 09:53:36 +0530

    Add files via upload

---
+++ Database/demo_scripts/insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO EMPLOYEE_DEMO (EMP_NAME, EMAIL, DEPARTMENT, SALARY)
+VALUES ('Deep Mondal', 'deep.mondal@example.com', 'IT', 75000);
+
+INSERT INTO EMPLOYEE_DEMO (EMP_NAME, EMAIL, DEPARTMENT, SALARY)
+VALUES ('Ankita Roy', 'ankita.roy@example.com', 'HR', 55000);
+
+INSERT INTO EMPLOYEE_DEMO (EMP_NAME, EMAIL, DEPARTMENT, SALARY)
+VALUES ('Rahul Sen', 'rahul.sen@example.com', 'Finance', 68000);
